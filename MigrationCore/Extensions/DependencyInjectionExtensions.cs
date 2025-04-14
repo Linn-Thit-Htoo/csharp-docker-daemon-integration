22b61a373dfac7b9031bed230a6c093932a5bf12
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-14 20:54:08 +0630

    Add project files.

---
+++ MigrationCore/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MigrationCore.AppDbContextModels;
+using MigrationCore.Services;
+
+namespace MigrationCore.Extensions
+{
+    public static class DependencyInjectionExtensions
+    { 
+        public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers().AddJsonOptions(opt =>
+            {
+                opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+            });
+
+            builder.Services.AddDbContext<AppDbContext>(opt =>
+            {
+                opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddHealthChecks();
+            builder.Services.AddHostedService<MigrationHostedService>();
+
+            return services;
+        }
+    }
+}
---
+++ MigrationCore/Services/MigrationHostedService.cs
@@ -0,0 +1,51 @@
+﻿using EFCore.BulkExtensions;
+using Microsoft.EntityFrameworkCore;
+using MigrationCore.AppDbContextModels;
+
+namespace MigrationCore.Services
+{
+    public class MigrationHostedService : IHostedService
+    {
+        private readonly IServiceScopeFactory _serviceScopeFactory;
+        private readonly ILogger<MigrationHostedService> _logger;
+
+        public MigrationHostedService(IServiceScopeFactory serviceScopeFactory, ILogger<MigrationHostedService> logger)
+        {
+            _serviceScopeFactory = serviceScopeFactory;
+            _logger = logger;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                var lst = new List<TblBlog>();
+                var scope = _serviceScopeFactory.CreateScope();
+                var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+                for (int i = 1; i <= 20000; i++)
+                {
+                    lst.Add(new TblBlog()
+                    {
+                        BlogId = Guid.NewGuid().ToString(),
+                        BlogTitle = $"Blog Title {i}",
+                        BlogAuthor = $"Blog Author: {i}",
+                        BlogContent = $"Blog Content: {i}",
+                        IsDeleted = false
+                    });
+                }
+
+                await context.BulkInsertAsync(lst, cancellationToken: cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
