cfe1bebcd89d80f612184ad82dc50931414f0c83
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-15 07:26:09 +0630

    -

--- MigrationCore/Extensions/DependencyInjectionExtensions.cs
+++ MigrationCore/Extensions/DependencyInjectionExtensions.cs
@@ -2,30 +2,29 @@
 using MigrationCore.AppDbContextModels;
 using MigrationCore.Services;
 
-namespace MigrationCore.Extensions
-{
-    public static class DependencyInjectionExtensions
-    { 
-        public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
+namespace MigrationCore.Extensions;
+
+public static class DependencyInjectionExtensions
+{ 
+    public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers().AddJsonOptions(opt =>
         {
-            builder.Services.AddControllers().AddJsonOptions(opt =>
-            {
-                opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-            });
+            opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+        });
 
-            builder.Services.AddDbContext<AppDbContext>(opt =>
-            {
-                opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-            });
+        builder.Services.AddDbContext<AppDbContext>(opt =>
+        {
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+            opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        });
 
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddHealthChecks();
-            builder.Services.AddHostedService<MigrationHostedService>();
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddHealthChecks();
+        builder.Services.AddHostedService<MigrationHostedService>();
 
-            return services;
-        }
+        return services;
     }
 }
