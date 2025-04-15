ffd76f9a2dd97c1c28ca280209ac2be8b9e38129
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-15 14:52:29 +0630

    -

--- PublicApi/Extensions/DependencyInjectionExtensions.cs
+++ PublicApi/Extensions/DependencyInjectionExtensions.cs
@@ -1,26 +1,25 @@
-﻿namespace PublicApi.Extensions
+﻿namespace PublicApi.Extensions;
+
+public static class DependencyInjectionExtensions
 {
-    public static class DependencyInjectionExtensions
+    public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
+        builder.Services.AddControllers().AddJsonOptions(opt =>
         {
-            builder.Services.AddControllers().AddJsonOptions(opt =>
+            opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+        });
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddHealthChecks();
+
+        builder.Services.AddHttpClient(
+            "DockerClient",
+            opt =>
             {
-                opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-            });
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddHealthChecks();
+                opt.BaseAddress = new Uri("http://localhost:5555");
+            }
+        );
 
-            builder.Services.AddHttpClient(
-                "DockerClient",
-                opt =>
-                {
-                    opt.BaseAddress = new Uri("http://localhost:5555");
-                }
-            );
-
-            return services;
-        }
+        return services;
     }
 }
